8349ff6bd1b812cc6f4aa5354f1a9cc790e34929
anbi <biswaszin@gmail.com>
2026-06-15 02:52:01 +0530

    add: opendir

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <dirent.h>
 
 int main (int argc, char *argv[]){
+
     const char * path = (argc > 1) ? argv[1]: ".";
-    printf("%s \n", path);
+
+    DIR *dir = opendir(path);
+    // printf("%s \n", path);
+
     return 0;
 }
