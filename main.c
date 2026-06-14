4ccb5a02b0551417880dd49955a6e41de119e5b8
anbi <biswaszin@gmail.com>
2026-06-15 03:05:45 +0530

    add: dirent entry point

--- main.c
+++ main.c
@@ -6,7 +6,14 @@ int main (int argc, char *argv[]){
     const char * path = (argc > 1) ? argv[1]: ".";
 
     DIR *dir = opendir(path);
-    // printf("%s \n", path);
+    if (!dir) {
+        perror("opendir");
+        return 1;
+    }
+
+    struct dirent *entry;
+    entry = readdir(dir);
+    printf("%s\n", entry->d_name);
 
     return 0;
 }
