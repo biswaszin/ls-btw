afcfc634abd5c41093542b8581cecd3fe654ce5a
anbi <biswaszin@gmail.com>
2026-06-15 22:04:33 +0530

    update: basic working of ls

--- main.c
+++ main.c
@@ -3,17 +3,21 @@
 
 int main (int argc, char *argv[]){
 
-    const char * path = (argc > 1) ? argv[1]: ".";
+    const char *path = (argc > 1) ? argv[1]: "."; // Getting the path
 
-    DIR *dir = opendir(path);
-    if (!dir) {
-        perror("opendir");
-        return 1;
+    DIR *dir = opendir(path); // opening the path, storing the address in dir
+    if (!dir) {  // checking if the path is null or not
+        perror("opendir"); // if doesn't exist, we'll print an error
+        return 1; // exiting the code
     }
 
     struct dirent *entry;
-    entry = readdir(dir);
-    printf("%s\n", entry->d_name);
+    while ((entry = readdir(dir)) != NULL){
+        if (entry->d_name[0] == '.') continue;
+        printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
 
     return 0;
 }
